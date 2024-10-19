5e6108001784a80c9a92a603f4cb033afe247a76
jijihome <jijihome@gmail.com>
2024-10-19 13:55:48 +0800

    在.prettierc.js中配置换行符为LF
    在.prettierc.js文件中添加了endOfLine选项，并设置为'lf'，以确保代码的换行符使用LF（Unix风格）。

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,7 +3,8 @@ module.exports = {
   semi: true, // 在语句末尾添加分号
   bracketSpacing: true, // 在大括号内添加空格，例如 { foo: bar }
   tabWidth: 2, // 使用2个空格进行缩进
-  useTabs: false, // 不使用tab进行缩进
+  useTabs: false, // 不使用 tab 进行缩进
   trailingComma: 'all', // 在对象和数组末尾添加逗号
   arrowParens: 'always', // 在箭头函数参数周围添加括号
+  endOfLine: 'lf', // 使用 LF 作为换行符
 };
