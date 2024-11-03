a9f1a4b6bb2c7b6ab028f9527169a8b3422852db
jijihome <jijihome@gmail.com>
2024-11-03 13:29:06 +0800

    feat(preload): 实现渲染进程和主进程的通信方法
    - 新增了调用错误框函数，用于在渲染进程中显示错误消息框
    - 在 preload.js 中暴露了一组 API，包括监听、移除监听、发送、调用、发送同步等方法
    - 这些方法封装了 ipcRenderer 的功能，便于渲染进程和主进程之间的通信

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,1 +1,54 @@
-const { contextBridge } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron');
+
+const 调用错误框 = (error) => {
+  ipcRenderer.invoke('ui:错误消息框', error.message);
+};
+
+// 暴露给渲染进程的 API
+contextBridge.exposeInMainWorld('进程通信', {
+  // 从渲染器进程到主进程的异步通信
+
+  /**************************************************
+   * 通用方法
+   ***************************************************/
+  监听: (channel, func) => {
+    try {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    } catch (error) {
+      调用错误框(error.message);
+      throw error;
+    }
+  },
+  移除监听: (channel, ...args) => {
+    try {
+      ipcRenderer.removeListener(channel, ...args);
+    } catch (error) {
+      调用错误框(error.message);
+      throw error;
+    }
+  },
+  发送: (channel, ...args) => {
+    try {
+      ipcRenderer.send(channel, ...args);
+    } catch (error) {
+      调用错误框(error.message);
+      throw error;
+    }
+  },
+  调用: (channel, ...args) => {
+    try {
+      return ipcRenderer.invoke(channel, ...args);
+    } catch (error) {
+      调用错误框(error.message);
+      throw error;
+    }
+  },
+  发送同步: (channel, ...args) => {
+    try {
+      return ipcRenderer.sendSync(channel, ...args);
+    } catch (error) {
+      调用错误框(error.message);
+      throw error;
+    }
+  },
+});
